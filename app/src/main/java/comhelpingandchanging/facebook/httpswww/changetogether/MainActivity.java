c36a8b194619cf0a4aa7dae8cc25cb6c569e00d0
iBuaRecords <daywalker@pferr.de>
2016-10-17 15:55:40 +0200

    Button mit OnClick

--- app/src/main/java/comhelpingandchanging/facebook/httpswww/changetogether/MainActivity.java
+++ app/src/main/java/comhelpingandchanging/facebook/httpswww/changetogether/MainActivity.java
@@ -1,15 +1,29 @@
 package comhelpingandchanging.facebook.httpswww.changetogether;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //Test
         // Test Len
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent search = new Intent(MainActivity.this, SearchActivity.class);
+                startActivity(search);
+            }
+        });
     }
 }
---
+++ app/src/main/java/comhelpingandchanging/facebook/httpswww/changetogether/SearchActivity.java
@@ -0,0 +1,11 @@
+package comhelpingandchanging.facebook.httpswww.changetogether;
+
+import android.app.Activity;
+
+/**
+ * Created by Yannick on 17.10.2016.
+ */
+
+public class SearchActivity extends Activity {
+
+}
