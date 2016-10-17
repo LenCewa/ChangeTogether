bca9ec2b937ba85cb37e78f424f6135bd2810565
Len <len13@hotmail.de>
2016-10-17 16:51:57 +0200

    Menu Class

---
+++ app/src/main/java/comhelpingandchanging/facebook/httpswww/changetogether/MenuActivity.java
@@ -0,0 +1,16 @@
+package comhelpingandchanging.facebook.httpswww.changetogether;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by len13 on 17.10.2016.
+ */
+
+public class MenuActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
--- app/src/main/java/comhelpingandchanging/facebook/httpswww/changetogether/SearchActivity.java
+++ app/src/main/java/comhelpingandchanging/facebook/httpswww/changetogether/SearchActivity.java
@@ -1,17 +1,32 @@
 package comhelpingandchanging.facebook.httpswww.changetogether;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Yannick on 17.10.2016.
  */
 
 public class SearchActivity extends Activity {
+    Button menu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        
+        menu = (Button) findViewById(R.id.menu);
+        menu.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent menuActivity = new Intent(SearchActivity.this, MenuActivity.class);
+                startActivity(menuActivity);
+            }
+        });
     }
 
 }
