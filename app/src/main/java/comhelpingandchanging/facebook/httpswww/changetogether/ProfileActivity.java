5e2427a8ca4519e5e11822c353b4d2c4212e13cf
iBuaRecords <daywalker@pferr.de>
2016-10-18 11:58:41 +0200

    Feedback Liste hinzugefügt

--- app/src/main/java/comhelpingandchanging/facebook/httpswww/changetogether/ProfileActivity.java
+++ app/src/main/java/comhelpingandchanging/facebook/httpswww/changetogether/ProfileActivity.java
@@ -1,10 +1,17 @@
 package comhelpingandchanging.facebook.httpswww.changetogether;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by len13 on 17.10.2016.
@@ -12,6 +19,11 @@ import android.widget.Button;
 
 public class ProfileActivity extends Activity {
     Button menu;
+    Button feedbackButton;
+    ListView feedbackList;
+    EditText feedback;
+    ArrayList<String> listItems=new ArrayList<String>();
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +39,30 @@ public class ProfileActivity extends Activity {
                 startActivity(menuActivity);
             }
         });
+
+        feedbackList = (ListView) findViewById(R.id.list);
+        adapter=new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listItems);
+        feedbackList.setAdapter(adapter);
+
+        feedback = (EditText) findViewById(R.id.editText);
+
+
+
+        feedbackButton = (Button) findViewById(R.id.sendButton);
+        feedbackButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Editable feedbackText = feedback.getText();
+                listItems.add(feedbackText.toString());
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+
+
+
     }
 }
