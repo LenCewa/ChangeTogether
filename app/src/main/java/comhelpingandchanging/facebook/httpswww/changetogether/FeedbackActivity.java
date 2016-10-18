e819e5ccd9cb2d228f4f7fb47a977b4aa9071d9b
Len <len13@hotmail.de>
2016-10-18 15:46:08 +0200

    Feedback Activity

---
+++ app/src/main/java/comhelpingandchanging/facebook/httpswww/changetogether/FeedbackActivity.java
@@ -0,0 +1,20 @@
+package comhelpingandchanging.facebook.httpswww.changetogether;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+/**
+ * Created by len13 on 18.10.2016.
+ */
+
+public class FeedbackActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+    }
+}
