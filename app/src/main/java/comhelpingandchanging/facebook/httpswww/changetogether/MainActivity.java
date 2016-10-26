1eeeb52ecec42799a5aac891d6a06997a2f87df3
Len <len13@hotmail.de>
2016-10-26 14:40:05 +0200

    update Login und DB

--- app/src/main/java/comhelpingandchanging/facebook/httpswww/changetogether/MainActivity.java
+++ app/src/main/java/comhelpingandchanging/facebook/httpswww/changetogether/MainActivity.java
@@ -5,10 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     //Push
     Button login;
+    EditText email, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
