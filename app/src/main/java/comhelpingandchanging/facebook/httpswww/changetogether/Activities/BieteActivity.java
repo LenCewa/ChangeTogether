31897d65b86be0225444deb67f989a6e8c07645b
Len <len13@hotmail.de>
2016-10-28 23:44:05 +0200

    Biete ListView funktioniert noch nicht

--- app/src/main/java/comhelpingandchanging/facebook/httpswww/changetogether/Activities/BieteActivity.java
+++ app/src/main/java/comhelpingandchanging/facebook/httpswww/changetogether/Activities/BieteActivity.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -16,7 +18,9 @@ import comhelpingandchanging.facebook.httpswww.changetogether.R;
 
 public class BieteActivity extends Activity {
     Button menu, add;
-    ArrayList<String> bieteItems = new ArrayList<>();
+    ArrayList<String> bieteItems;
+    ListView bieteList;
+    customAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,5 +47,17 @@ public class BieteActivity extends Activity {
                 startActivity(addActivity);
             }
         });
+
+        bieteItems = getIntent().getParcelableExtra("bieteItems");
+        bieteList = (ListView) findViewById(R.id.bieteList);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, 0, bieteItems); // 0 ist falsch
+
+        /*if (bieteItems != null) {
+            for (int i = 0; i < bieteItems.size(); i++) {
+                String[] data = bieteItems.get(i);
+                bieteLis
+            }
+        }*/
+
     }
 }
