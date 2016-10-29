b02f78b3a139d1e276745204d0494f09f739b425
LycketySplit <lucky.koch@gmx.de>
2016-10-29 20:13:55 +0200

    - Passwortüberprüfung in Settings

--- app/src/main/java/comhelpingandchanging/facebook/httpswww/changetogether/Activities/SettingsActivity.java
+++ app/src/main/java/comhelpingandchanging/facebook/httpswww/changetogether/Activities/SettingsActivity.java
@@ -2,19 +2,16 @@ package comhelpingandchanging.facebook.httpswww.changetogether.Activities;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 import comhelpingandchanging.facebook.httpswww.changetogether.R;
+import comhelpingandchanging.facebook.httpswww.changetogether.Utilities.Account;
 
 /**
  * Created by Ludwig on 29.10.2016.
@@ -22,6 +19,9 @@ import comhelpingandchanging.facebook.httpswww.changetogether.R;
 
 public class SettingsActivity extends Activity {
     Button save;
+    Account account;
+    EditText location;
+    EditText language;
     EditText password;
     EditText passwordConfirm;
 
@@ -31,20 +31,29 @@ public class SettingsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        account = (Account) getApplication();
         save = (Button) findViewById(R.id.Save);
+        location = (EditText) findViewById(R.id.changeLocation);
+        language = (EditText) findViewById(R.id.changeLanguage);
         password = (EditText) findViewById(R.id.changePassword);
         passwordConfirm = (EditText) findViewById(R.id.ConfirmPassword);
+
+        location.setText(account.getLocation());
+        language.setText(account.getLanguage());
 
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(password.getText().toString().equals(passwordConfirm.getText().toString())) {
-                    Intent OwnprofileActivity = new Intent(SettingsActivity.this, OwnProfileActivity.class);
-                    startActivity(OwnprofileActivity);
-                    finish();
+                if (password.getText().toString().length() != 0 && passwordConfirm.getText().toString().length() != 0) {
+                    if (password.getText().toString().equals(passwordConfirm.getText().toString())) {
+                        Intent OwnprofileActivity = new Intent(SettingsActivity.this, OwnProfileActivity.class);
+                        startActivity(OwnprofileActivity);
+                        finish();
+                    } else
+                        Toast.makeText(SettingsActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
                 }
                 else
-                    Toast.makeText(SettingsActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(SettingsActivity.this, "Passwords can not be empty", Toast.LENGTH_SHORT).show();
             }
         });
 
