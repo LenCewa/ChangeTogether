b136ca76957dcf5f1251ac21f6f1fe6d3f1ce3b0
iBuaRecords <daywalker@pferr.de>
2016-11-02 22:48:58 +0100

    Bilder werden jetzt angezeigt

--- app/src/main/java/comhelpingandchanging/facebook/httpswww/changetogether/Activities/SettingsActivity.java
+++ app/src/main/java/comhelpingandchanging/facebook/httpswww/changetogether/Activities/SettingsActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -25,6 +26,7 @@ public class SettingsActivity extends Activity {
     EditText language;
     EditText password;
     EditText passwordConfirm;
+    ImageView profilePic;
 
     private GoogleApiClient client;
 
@@ -38,9 +40,17 @@ public class SettingsActivity extends Activity {
         language = (EditText) findViewById(R.id.changeLanguage);
         password = (EditText) findViewById(R.id.changePassword);
         passwordConfirm = (EditText) findViewById(R.id.ConfirmPassword);
+        profilePic = (ImageView) findViewById(R.id.changeProfilePic);
 
         location.setText(account.getLocation());
         language.setText(account.getLanguage());
+
+        profilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
 
         save.setOnClickListener(new View.OnClickListener() {
             @Override
