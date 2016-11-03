a7a902715e0ee0123e8a92d87faf6f3629182494
Len <len13@hotmail.de>
2016-11-03 15:59:19 +0100

    locations in listview temporär gespeichert

--- app/src/main/java/comhelpingandchanging/facebook/httpswww/changetogether/Activities/HelpingLocationsActivity.java
+++ app/src/main/java/comhelpingandchanging/facebook/httpswww/changetogether/Activities/HelpingLocationsActivity.java
@@ -2,12 +2,18 @@ package comhelpingandchanging.facebook.httpswww.changetogether.Activities;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 import comhelpingandchanging.facebook.httpswww.changetogether.R;
 import comhelpingandchanging.facebook.httpswww.changetogether.Utilities.Account;
@@ -17,8 +23,12 @@ import comhelpingandchanging.facebook.httpswww.changetogether.Utilities.Account;
  */
 
 public class HelpingLocationsActivity extends Activity {
-    Button menu;
+    Button menu, addLocation;
     Account account;
+    TextView locationName, addedLocations;
+    ListView addedLocationsList;
+    ArrayAdapter<String> adapter;
+    ArrayList<String> usersHelpingLocations = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +39,7 @@ public class HelpingLocationsActivity extends Activity {
         AutoCompleteTextView autocompleteView = (AutoCompleteTextView) findViewById(R.id.autocomplete);
         autocompleteView.setAdapter(new PlacesAutoCompleteAdapter(this, R.layout.autocomplete_list_item)); // vorher getActivity() anstelle von this
 
-        menu = (Button) findViewById(R.id.button2);
+        menu = (Button) findViewById(R.id.button4);
         menu.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -39,14 +49,46 @@ public class HelpingLocationsActivity extends Activity {
             }
         });
 
-        autocompleteView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        /*autocompleteView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 // Get data associated with the specified position
                 // in the list (AdapterView)
                 String description = (String) parent.getItemAtPosition(position);
-                //Toast.makeText(this, description, Toast.LENGTH_SHORT).show(); // TODO vorher getActivity() anstelle von this
+                //Toast.makeText(this, description, Toast.LENGTH_SHORT).show(); // vorher getActivity() anstelle von this
+            }
+        });*/
+
+        locationName = (TextView) findViewById(R.id.autocomplete);
+        addLocation = (Button) findViewById(R.id.addLocationBtn);
+        addedLocations = (TextView) findViewById(R.id.addedLocationTextView);
+
+        //ListView vorbereiten
+        addedLocationsList = (ListView) findViewById(R.id.addedLocationsListView);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, usersHelpingLocations);
+        addedLocationsList.setAdapter(adapter);
+        //ListView vorbereitet
+
+        addLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String location = locationName.getText().toString();
+                if (!isDuplicate(location)) {
+                    usersHelpingLocations.add(location);
+                    Log.e("autocomplete", location);
+                    adapter.notifyDataSetChanged();
+                }
+                else
+                    Log.e("adding a dupl. location", location);
             }
         });
+
+    }
+
+    public boolean isDuplicate(String location) {
+        for (String loc : usersHelpingLocations)
+            if (loc.equals(location))
+                return true;
+        return false;
     }
 }
