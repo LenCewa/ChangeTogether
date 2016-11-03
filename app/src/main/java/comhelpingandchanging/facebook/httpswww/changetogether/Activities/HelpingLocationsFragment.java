1f8af8107e1818224304a0401618be31e8d211e5
iBuaRecords <daywalker@pferr.de>
2016-11-03 14:35:06 +0100

    Activities in Fragments geändert

--- app/src/main/java/comhelpingandchanging/facebook/httpswww/changetogether/Activities/HelpingLocationsFragment.java
+++ app/src/main/java/comhelpingandchanging/facebook/httpswww/changetogether/Activities/HelpingLocationsFragment.java
@@ -1,43 +1,42 @@
 package comhelpingandchanging.facebook.httpswww.changetogether.Activities;
 
-import android.app.Activity;
+import android.app.Fragment;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import comhelpingandchanging.facebook.httpswww.changetogether.R;
 import comhelpingandchanging.facebook.httpswww.changetogether.Utilities.Account;
 
 /**
- * Created by len13 on 29.10.2016.
+ * Created by Yannick on 03.11.2016.
  */
 
-public class HelpingLocationsActivity extends Activity {
+public class HelpingLocationsFragment extends Fragment {
+
+    View view;
     Button menu;
     Account account;
 
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_helping_locations);
-
-        account = (Account) getApplication();
-        AutoCompleteTextView autocompleteView = (AutoCompleteTextView) findViewById(R.id.autocomplete);
-        autocompleteView.setAdapter(new PlacesAutoCompleteAdapter(this, R.layout.autocomplete_list_item)); // vorher getActivity() anstelle von this
-
-        menu = (Button) findViewById(R.id.button2);
-        menu.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                Intent menuActivity = new Intent(HelpingLocationsActivity.this, MenuActivity.class);
-                startActivity(menuActivity);
-            }
-        });
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.activity_biete, container, false);
+        account = (Account) getActivity().getApplication();
+        AutoCompleteTextView autocompleteView = (AutoCompleteTextView) view.findViewById(R.id.autocomplete);
+        autocompleteView.setAdapter(new PlacesAutoCompleteAdapter(getActivity(), R.layout.autocomplete_list_item)); // vorher getActivity() anstelle von this
 
         autocompleteView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -48,5 +47,6 @@ public class HelpingLocationsActivity extends Activity {
                 //Toast.makeText(this, description, Toast.LENGTH_SHORT).show(); // TODO vorher getActivity() anstelle von this
             }
         });
+        return view;
     }
 }
