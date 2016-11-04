d4bbd6b30b52a5fdbc2dd9c0b668c9aa201f3374
Len <len13@hotmail.de>
2016-11-04 09:29:42 +0100

    DB-Table hinzugefügt und Klassen erstellt

---
+++ app/src/main/java/comhelpingandchanging/facebook/httpswww/changetogether/Utilities/DeleteHelpingLocations.java
@@ -0,0 +1,19 @@
+package comhelpingandchanging.facebook.httpswww.changetogether.Utilities;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by len13 on 04.11.2016.
+ */
+
+public class DeleteHelpingLocations  extends AsyncTask<Void, Void, String> {
+    @Override
+    protected String doInBackground(Void... params) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+
+    }
+}
