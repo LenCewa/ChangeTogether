2d2efe5792d2477837ad1a19ff65516fea8055d0
LycketySplit <lucky.koch@gmx.de>
2016-11-05 17:22:20 +0100

    ShowBidFeedback Activity

---
+++ app/src/main/java/comhelpingandchanging/facebook/httpswww/changetogether/Activities/ShowBidFeedback.java
@@ -0,0 +1,26 @@
+package comhelpingandchanging.facebook.httpswww.changetogether.Activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import comhelpingandchanging.facebook.httpswww.changetogether.R;
+import comhelpingandchanging.facebook.httpswww.changetogether.Utilities.Account;
+
+/**
+ * Created by Ludwig on 05.11.2016.
+ */
+
+public class ShowBidFeedback extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+}
+}
