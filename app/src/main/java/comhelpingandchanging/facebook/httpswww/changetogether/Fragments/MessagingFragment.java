8b154b4b6387861df804120437f6dc2d0f29f270
Len <len13@hotmail.de>
2016-11-11 21:31:06 +0100

    inbox

---
+++ app/src/main/java/comhelpingandchanging/facebook/httpswww/changetogether/Fragments/MessagingFragment.java
@@ -0,0 +1,10 @@
+package comhelpingandchanging.facebook.httpswww.changetogether.Fragments;
+
+import android.app.Fragment;
+
+/**
+ * Created by len13 on 11.11.2016.
+ */
+
+public class MessagingFragment extends Fragment {
+}
