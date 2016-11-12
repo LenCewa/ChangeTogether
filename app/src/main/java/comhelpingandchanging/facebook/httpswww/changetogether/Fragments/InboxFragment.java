9280cf990acdcbfefa6038e4276529e9760ee364
Len <len13@hotmail.de>
2016-11-12 11:59:28 +0100

    ...

--- app/src/main/java/comhelpingandchanging/facebook/httpswww/changetogether/Fragments/InboxFragment.java
+++ app/src/main/java/comhelpingandchanging/facebook/httpswww/changetogether/Fragments/InboxFragment.java
@@ -2,28 +2,50 @@ package comhelpingandchanging.facebook.httpswww.changetogether.Fragments;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.Api;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.concurrent.TimeUnit;
 
 import comhelpingandchanging.facebook.httpswww.changetogether.R;
 import comhelpingandchanging.facebook.httpswww.changetogether.Utilities.Account;
-//import de.hdodenhof.circleimageview.CircleImageView;
+
+//Tutorial Impoprts
+
+import com.google.android.gms.auth.api.Auth;
+import de.hdodenhof.circleimageview.CircleImageView;
 /**
  * Created by len13 on 11.11.2016.
  *
  * https://codelabs.developers.google.com/codelabs/firebase-android/#0
  */
 
-public class InboxFragment extends Fragment /*implements GoogleApiClient.OnConnectionFailedListener */{
+public class InboxFragment extends Fragment implements GoogleApiClient.OnConnectionFailedListener {
 
-    /*public static class MessageViewHolder extends RecyclerView.ViewHolder {
+    public static class MessageViewHolder extends RecyclerView.ViewHolder {
         public TextView messageTextView;
         public TextView messengerTextView;
         public CircleImageView messengerImageView;
@@ -34,8 +56,27 @@ public class InboxFragment extends Fragment /*implements GoogleApiClient.OnConne
             messengerTextView = (TextView) itemView.findViewById(R.id.messengerTextView);
             messengerImageView = (CircleImageView) itemView.findViewById(R.id.messengerImageView);
         }
-    }*/
+    }
 
+    private static final String TAG = "InboxFragment";
+    public static final String MESSAGES_CHILD = "messages";
+    private static final int REQUEST_INVITE = 1;
+    public static final int DEFAULT_MSG_LENGTH_LIMIT = 10;
+    public static final String ANONYMOUS = "anonymous";
+    private static final String MESSAGE_SENT_EVENT = "message_sent";
+    private String mUsername;
+    private String mPhotoUrl;
+    private SharedPreferences mSharedPreferences;
+    private GoogleApiClient mGoogleApiClient;
+
+    private Button mSendButton;
+    private RecyclerView mMessageRecyclerView;
+    private LinearLayoutManager mLinearLayoutManager;
+    private ProgressBar mProgressBar;
+    private EditText mMessageEditText;
+
+
+    // My classical declarations
     View view;
     Activity callingActivity;
     Account account;
@@ -44,12 +85,21 @@ public class InboxFragment extends Fragment /*implements GoogleApiClient.OnConne
         setArguments(new Bundle());
     }
 
+
+    // Firebase instance variables
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.activity_inbox, container, false);
         callingActivity = getActivity();
-        account = (Account) callingActivity.getApplication();
+        //account = (Account) callingActivity.getApplication();
+
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        mUsername = ANONYMOUS;
+
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity()).addApi(Auth.GOOGLE_SIGN_IN_API).build();
+
         return view;
     }
 
@@ -67,14 +117,25 @@ public class InboxFragment extends Fragment /*implements GoogleApiClient.OnConne
         super.onResume();
         refresh();
     }
-    /*
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    public void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
     // Interface...
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
         // An unresolvable error has occurred and Google APIs (including Sign-In) will not
         // be available.
         Log.d(TAG, "onConnectionFailed:" + connectionResult);
-        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
-    }*/
+        //Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
+    }
 
 }
