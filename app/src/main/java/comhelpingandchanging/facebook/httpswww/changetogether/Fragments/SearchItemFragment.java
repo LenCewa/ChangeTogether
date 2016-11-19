78d030c8a11e7f5fded0363fd5d92e4e7516dba1
LycketySplit <lucky.koch@gmx.de>
2016-11-19 23:24:08 +0100

    Teilnehmen Button

--- app/src/main/java/comhelpingandchanging/facebook/httpswww/changetogether/Fragments/SearchItemFragment.java
+++ app/src/main/java/comhelpingandchanging/facebook/httpswww/changetogether/Fragments/SearchItemFragment.java
@@ -12,10 +12,12 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -36,6 +38,7 @@ public class SearchItemFragment extends Fragment{
     View view;
     Activity callingActivity;
     Account account;
+    Button join;
     ImageView userProfile;
     TextView userEmail;
     TextView userBid;
@@ -55,6 +58,7 @@ public class SearchItemFragment extends Fragment{
         userDescription = (TextView) view.findViewById(R.id.userDescription);
         ratingBar = (RatingBar) view.findViewById(R.id.avergageRating);
         ratings = (TextView) view.findViewById(R.id.rezensionen);
+        join = (Button) view.findViewById(R.id.joinButton);
 
         userProfile.setImageBitmap(account.getSearchProfilePic());
         userEmail.setText(account.getSearchEmail());
@@ -66,6 +70,13 @@ public class SearchItemFragment extends Fragment{
         ((CollapsingToolbarLayout)getActivity().findViewById(R.id.collapsing_toolbar)).setTitleEnabled(false);
         ((TextView)getActivity().findViewById(R.id.toolbar_title)).setText("Angebot von " + account.getSearchEmail());
         ((ImageView)getActivity().findViewById(R.id.ownProfilePic)).setImageBitmap(null);
+
+        join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("TODO", "An DB senden");
+            }
+        });
 
         ratingBar.setOnTouchListener(new View.OnTouchListener() {
             @Override
