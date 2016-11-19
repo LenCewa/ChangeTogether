4c26a7c800e71027998660dce2614354f0e75d5d
Len <len13@hotmail.de>
2016-11-19 14:46:47 +0100

    weiter gemacht

---
+++ app/src/main/java/comhelpingandchanging/facebook/httpswww/changetogether/Utilities/MyFirebaseInstanceIdService.java
@@ -0,0 +1,10 @@
+package comhelpingandchanging.facebook.httpswww.changetogether.Utilities;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by len13 on 19.11.2016.
+ */
+
+public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+}
---
+++ app/src/main/java/comhelpingandchanging/facebook/httpswww/changetogether/Utilities/MyFirebaseMessagingService.java
@@ -0,0 +1,10 @@
+package comhelpingandchanging.facebook.httpswww.changetogether.Utilities;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * Created by len13 on 19.11.2016.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+}
